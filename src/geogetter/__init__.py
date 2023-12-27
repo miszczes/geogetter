4a8ef52032b233413d46730bf0f4e2143615c4e1
Mikolaj Szczesny <miszczes0@gmail.com>
2023-12-27 21:30:30 +0100

    Version 0.2.0 update
    fetch more information from Nominatim
    
    ablility to fetch entire address dict from API
    
    Signed-off-by: Mikolaj Szczesny <miszczes0@gmail.com>

--- src/geogetter/__init__.py
+++ src/geogetter/__init__.py
@@ -12,18 +12,23 @@ import requests
 
 class Geocoder:
 
-    def __init__(self, address_details: str):
+    def __init__(self, address_details: str, address_json=False):
         """
         geogetter object instance creation
 
         :param address_details: Searched address details (could be just name of the city)
         :type address_details: str
+        :param address_json: If true geogetter also fetches dictionary with a
+        :type address_json: bool
         """
-        coordinates = self.__use_nominatim_api(address_details)
-        self.lat, self.lon = coordinates
+        coordinates = self.__use_nominatim_api(address_details, address_json)
+        if address_json:
+            self.target_name, self.lat, self.lon, self.address_json = coordinates
+        else:
+            self.target_name, self.lat, self.lon = coordinates
 
     @staticmethod
-    def __use_nominatim_api(address_details: str):
+    def __use_nominatim_api(address_details: str, address_json: bool):
         """
         Requests call to use Nominatim API
         through address details with JSON response
@@ -42,7 +47,10 @@ class Geocoder:
         )
         if response.status_code == 200:
             resp = dict(response.json()[0])
-            return resp.get("lat"), resp.get("lon")
+            result = [resp.get("display_name"), resp.get("lat"), resp.get("lon")]
+            if address_json:
+                result.append(resp.get("address"))
+            return result
         else:
             raise ValueError(f"Status code: {response.status_code};"
                              " Could not connect to given http address")
